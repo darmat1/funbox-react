b4883da465fab66c62d4ee437975355be8e8bf64
Andrew Kupriyanov <andrew.darmat@gmail.com>
2019-09-03 23:05:50 +0300

    isomorphic-fetch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import './App.scss';
 import ProductList from './components/ProductList';
 
+
 class App extends Component {
   constructor() {
     super();
@@ -12,6 +13,9 @@ class App extends Component {
 
   }
   fetchData = (url) => {
+    require('es6-promise').polyfill();
+    require('isomorphic-fetch');
+    
     fetch(url)
       .then((response) => response.json())
       .then((data) => this.setState({ products: data })
@@ -25,10 +29,10 @@ class App extends Component {
   }
   render() {
     return (
-        <div className="wrapper">
-          <h1>Ты сегодня покормил кота?</h1>
-          <ProductList products={this.state.products} />
-        </div>
+      <div className="wrapper">
+        <h1>Ты сегодня покормил кота?</h1>
+        <ProductList products={this.state.products} />
+      </div>
     );
   }
 }
